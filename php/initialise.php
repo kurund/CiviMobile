081fcf9437fa9124af6c1e10ec246e9efecb01a9
kurund <kurund@civicrm.org>
2012-09-04 00:01:36 +0530

    delete unwanted file

--- php/initialise.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '/var/www/drupal/sites/default/civicrm.settings.php';
-//require_once '/clients/mobile/drupal/sites/all/modules/civicrm/CRM/Core/Config.php';
-require_once '/var/www/drupal/sites/all/modules/civicrm/api/api.php';
-//CRM_Core_Config::singleton( );
-
-?>
