9d1528d2f7a72e58169a82a2f61ea87896d34f85
Kurund Jalmi <me@kurund.com>
2012-09-21 19:14:06 +0530

    code cleanup, delete unwanted files

--- code/civimobile.home.php
+++ code/civimobile.home.php
@@ -17,32 +17,18 @@ require_once 'civimobile.header.php';
 <!-- end of menu page -->
 
 <div data-role="page" id="contact-search" >
-    <?php require_once 'civimobile.contact_list.tpl.php'; ?>
-</div>
-<div data-role="page" id="add-contact-page" >
-    <?php require_once 'civimobile.contact.tpl.php'; ?>
-</div>
-
-
-<!-- start of contact-edit page -->
-<div data-role="page" id="contact-edit" >
-    <?php //require_once 'civimobile.contact.tpl.php'; ?>
-</div>
-<!-- end of contact-edit page -->
-
-<div data-role="page" id="contactslist" >
-  <?php //require_once 'civimobile.contact.search.tpl.php'; ?>
+    <?php require_once 'civimobile.contact_search.html'; ?>
 </div>
 
 <!-- start of event page -->
 <div data-role="page" id="events" >
-  <?php  require_once 'civimobile.event_list.tpl.php';?>
+  <?php  require_once 'civimobile.event_search.html';?>
 </div>
 <!-- end of event page -->
 
 <!-- start of survey page -->
 <div data-role="page" id="survey" >
-   <?php  require_once 'civimobile.survey_list.tpl.php';?>
+   <?php  require_once 'civimobile.survey_search.html';?>
 </div>
 <!-- end of survey page -->
 
