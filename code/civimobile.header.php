6a752e6c77b31326b5b588855aa5fae98f929957
Kurund Jalmi <me@kurund.com>
2012-09-21 19:50:14 +0530

    add mustache.js for template rendering

--- code/civimobile.header.php
+++ code/civimobile.header.php
@@ -10,20 +10,10 @@ $civimobile_vars['title']   = 'CiviMobile';
 //$civimobile_vars['favicon'] = theme_get_setting("toggle_favicon") ? "<link rel=\"shortcut icon\" href=\"". theme_get_setting("favicon") ."\" type=\"image/x-icon\"/>\n" : "";
 
 $config =& CRM_Core_Config::singleton();
-$civimobile_vars['civicrm_base']   = $config->userFrameworkResourceURL;
+$civimobile_vars['civicrm_base']   = $config->userFrameworkResourceURL . DIRECTORY_SEPARATOR;
 
 // extension include path
 $includePath = $config->extensionsURL . 'com.webaccessglobal.module.civimobile' . DIRECTORY_SEPARATOR;
-
-global $base_url;
-function navbar ($back = false) {
-  if ($back) { 
-    $ajax="true";
-  } 
-  else {
-    $ajax="false";
-  }  
-}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -36,6 +26,7 @@ function navbar ($back = false) {
     <link rel="stylesheet" href="<?php print $includePath;?>libraries/jquery.mobile-1.2.0-beta.1/jquery.mobile-1.2.0-beta.1.min.css" />
     <script src="<?php print $includePath; ?>libraries/jquery.mobile-1.2.0-beta.1/jquery-1.8.1.min.js"></script>
     <script src="<?php print $includePath; ?>libraries/jquery.mobile-1.2.0-beta.1/jquery.mobile-1.2.0-beta.1.js"></script>
+    <script src="<?php print $civimobile_vars['civicrm_base']; ?>packages/jquery/plugins/jquery.mustache.js"></script>
     <script src="<?php print $civimobile_vars['civicrm_base']; ?>js/rest.js"></script>
     <script>
       var crmajaxURL = '<?php print base_path(); ?>civicrm/ajax/rest';
