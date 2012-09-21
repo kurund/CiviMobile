ef5f81ef25c12e642978494405b57b89ee4abdbe
Kurund Jalmi <kurund@civicrm.org>
2012-09-22 05:01:23 +0530

    added event search

--- code/civimobile.home.php
+++ code/civimobile.home.php
@@ -3,31 +3,31 @@ require_once 'civimobile.header.php';
 ?>
 
 <!-- start of menu page -->
-<div data-role="page" id="menu">
+<div data-role="page" id="crm-jqm-home">
   <div data-role="header">
     <h1>CiviMobile</h1>
 	</div><!-- /header -->
 
 	<div data-role="content">	
-    <a data-role="button" data-icon="search" href="#contact-search" title="Contacts" class="icons" data-transition="slideup" >Contact</a>
-    <a data-role="button" data-icon="grid" href="#events" title="Events" class="icons" data-transition="slideup" >Events</a>
-    <a data-role="button" data-icon="info" href="#survey" title="Survey" class="icons" data-transition="slideup" >Survey</a>
-  </div><!-- /content --> 
+    <a data-role="button" data-icon="search" href="#crm-contact-search" title="Contacts" class="icons" data-transition="slideup" >Contact</a>
+    <a data-role="button" data-icon="grid" href="#crm-events" title="Events" class="icons" data-transition="slideup" >Events</a>
+    <a data-role="button" data-icon="info" href="#crm-survey" title="Survey" class="icons" data-transition="slideup" >Survey</a>
+  </div><!-- /content -->
 </div>
 <!-- end of menu page -->
 
-<div data-role="page" id="contact-search" >
+<div data-role="page" id="crm-contact-search" >
     <?php require_once 'civimobile.contact_search.html'; ?>
 </div>
 
 <!-- start of event page -->
-<div data-role="page" id="events" >
-  <?php  //require_once 'civimobile.event_search.html';?>
+<div data-role="page" id="crm-events" >
+  <?php require_once 'civimobile.event_search.html';?>
 </div>
 <!-- end of event page -->
 
 <!-- start of survey page -->
-<div data-role="page" id="survey" >
+<div data-role="page" id="crm-survey" >
    <?php  //require_once 'civimobile.survey_search.html';?>
 </div>
 <!-- end of survey page -->
